552bf866ba8385fa13868927c9c245c3e8fd9460
AARIN SAHAI <aarin2008sahai@gmail.com>
2023-12-23 23:07:24 +0530

    socket

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const http = require('http');
+const server = http.createServer(app);
+const PORT = process.env.PORT || 200;
+
 app.use(express.static(__dirname+"/public"));
 app.get('/',(req,res) => {
     res.sendFile('./index.html');
 });
-app.listen(PORT, function() {
-    console.log("Server started successfully")
+
+const io = require("socket.io")(server);
+
+io.on('connection', (socket)=>{
+    console.log(socket.id);
+});
+server.listen(PORT, function() {
+    console.log("Server started successfully at port"+PORT)
 })
